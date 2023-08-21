b3c5936ac281c96572aeb2eeba95f57b7956636c
swajp <stejskalmira@icloud.com>
2023-08-21 09:37:10 +0200

    responsivity

--- components/form.tsx
+++ components/form.tsx
@@ -37,16 +37,15 @@ export default function Form() {
   return (
     <section className="h-screen w-full flex items-center bg-black content-center justify-center">
       <form onSubmit={handleSubmit} action="" className="flex flex-col">
-        <p className="font-black text-[#FFFF00] text-xl md:text-4xl lg:text-5xl mb-2 md:mb-4">
+        <p className="font-black text-[#FFFF00] text-2xl md:text-4xl lg:text-5xl mb-2 md:mb-4">
           ZADEJTE 10 MÍSTNÍ KÓD
         </p>
         <input
           type="number"
           id="code"
-          placeholder="1234567890"
           onChange={(e) => setCode(e.target.value)}
           value={code}
-          className="bg-transparent text-white border-2 mb-4 placeholder:text-neutral-800 border-neutral-300  focus:ring-0 p-6 md:p-8 pl-3 md:pl-6 font-medium spacing tracking-wider text-xl md:text-5xl lg:text-6xl focus:outline-none [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+          className="bg-transparent text-white border-[3px] mb-4 placeholder:text-neutral-800 border-neutral-300  focus:ring-0 p-6 md:p-8 pl-3 md:pl-6 font-medium spacing tracking-wider text-xl md:text-5xl lg:text-6xl focus:outline-none [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
         />
         <div className="flex ">
           <button className="text-black bg-[#FFFF00] p-2 md:p-4 text-lg md:text-2xl font-bold max-w-lg  hover:bg-white  transition-all">
